a4200338202d94585e91eb1aeddbb18ce4374ed6
Asmaa Saleh <asmaatareq1999@gmail.com>
2024-02-20 21:27:23 +0200

    dotenv package

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors')
+// const dotenv=require('dotenv')
+// dotenv.config({path:'./.env'})
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const app = express();
