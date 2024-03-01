106a9c0bb949100ec73ebb8d8718c297480e6e53
Asmaa Saleh <asmaatareq1999@gmail.com>
2024-03-01 14:35:31 +0200

    add store file route to app file,and test the functions

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const usersRouter = require('./routes/users');
 const materialRouter = require('./routes/material');
 const toolRouter = require('./routes/tool');
 const skillRouter=require('./routes/skill');
+const storeRouter=require('./routes/store');
 const app = express();
 
 app.use(cors({origin:"*"}));
@@ -22,4 +23,5 @@ app.use('/users', usersRouter);
 app.use('/material',materialRouter);
 app.use('/tool',toolRouter);
 app.use('/skill',skillRouter);
+app.use('/store',storeRouter);
 module.exports = app;
